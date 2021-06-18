96f0f87f28fd241db578f653e2b18444dcbabee2
Blue Bean <sl8305@hotmail.com>
2021-06-18 15:12:05 -0700

    basic footer created with dummy text and target

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import './Footer.css';
+
+function Footer () {
+    return(
+        <div className='footer'>
+            <div className='footerLinks'>
+                <a className='footerLinksStyle' href='#'>LinkedIn</a>
+                <a className='footerLinksStyle' href='#'>Portfolio</a>
+                <a className='footerLinksStyle' href='#'>Github</a>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
