11b3a4b095f19d3aaeb68bb484516c36d9e6dc5d
seattle-mini <sl8305@hotmail.com>
2021-06-21 23:36:49 -0700

    dummy buttons for calculation components created and target div

--- src/App.js
+++ src/App.js
@@ -6,8 +6,22 @@ function App() {
   return (
     <div className="App">
       <Header/>
+
       <div className='content'>
-        Hello world. Time to convert.
+        <div className='description'>
+          <h1>Description</h1>
+          <p>Here users can convert between temperature, length, weight and volume between the US and Metric system.  The return values will be to the second decimal point. Conversion tables are provided help show the user how the results were calculated. </p>
+        </div>
+
+        <div className='buttonGroup'>
+          <button className='apple'> Temperature </button>
+          <button> Weight </button>
+          <button> Length </button>
+          <button> Volume </button>
+        </div>
+
+        <div className='target'></div>
+
       </div>
 
       <Footer/>
